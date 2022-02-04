f0fe9a390457939af31b5bf41bd3c23c1b0e4406
Kevin Elliott <kevin@welikeinc.com>
2022-02-03 20:06:20 -0800

    Another fix to the remote installer script

--- scripts/remote-install.sh
+++ scripts/remote-install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+HOME_PATH=`pwd`
+SOURCE_PATH="${HOME_PATH}/source"
 REPO_IMAGE_DIETPI="https://github.com/airframesio/image-dietpi.git"
 
-mkdir ~/source
+mkdir ${SOURCE_DIR}
 sudo apt update
 sudo apt install -y apt-utils git
 
 git clone $REPO_IMAGE_DIETPI ${SOURCE_PATH}/image-dietpi
-cd ~/source/image-dietpi
+cd ${SOURCE_PATH}/image-dietpi
 ./scripts/install.sh
