1861db60ca7a81a60370d736b06143aad8c939b3
Kevin Elliott <kevin@welikeinc.com>
2022-02-17 16:02:46 -0800

    More tuning

--- scripts/remote-install.sh
+++ scripts/remote-install.sh
@@ -5,9 +5,9 @@ echo "Airframes: Starting remote installer..."
 SOURCE_PATH="/opt/source"
 REPO_IMAGE_DIETPI="https://github.com/airframesio/image-dietpi.git"
 
-sudo mkdir ${SOURCE_PATH}
-DEBIAN_FRONTEND=noninteractive sudo apt-get update -qq < /dev/null > /dev/null
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -qq apt-utils git < /dev/null > /dev/null
+sudo mkdir -p ${SOURCE_PATH}
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq apt-utils git < /dev/null > /dev/null
 
 git clone $REPO_IMAGE_DIETPI ${SOURCE_PATH}/image-dietpi
 cd ${SOURCE_PATH}/image-dietpi
