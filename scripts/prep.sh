9e5ec542e32cb0297a1c829ec4a27bbcda420211
Kevin Elliott <kevin@welikeinc.com>
2022-02-17 16:30:09 -0800

    More tuning

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -5,4 +5,4 @@ echo "Airframes: Preparations"
 # Prep
 sudo mkdir /opt/source
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq < /dev/null > /dev/null
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq apt-utils wget unzip < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq apt-utils wget unzip lsb-release < /dev/null > /dev/null
